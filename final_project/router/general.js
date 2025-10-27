61c9dc8cb3f8bbc1d2c228ac2925ade5101417cd
Dklotz23 <devinmklotz@gmail.com>
2025-10-27 13:37:35 -0400

    updated getallbooks and get books by ISBN get methods

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,23 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+    return res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  for (i=0; keys.length; i++) {
+    if (keys[i][0].value == author)
+        return res.send(keys[i][0].value)
+  }
 });
 
 // Get all books based on title
