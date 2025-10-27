5968996cf2339cdf0e5951fba5b761cb704fa021
Dklotz23 <devinmklotz@gmail.com>
2025-10-27 12:52:38 -0400

    added auth session logic to index file.

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization["accesstoken"]) {
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+            }            
+            else {
+                res.status(404).json({ message: "User Not Authenticated" });
+            }
+        });
+    } else {
+            return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
