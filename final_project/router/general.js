5ed85b2e60e7d4ba4cdb247948227225f57f28f3
Dklotz23 <devinmklotz@gmail.com>
2025-10-28 10:16:07 -0400

    Added logic for registring user and isValid helper function.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,49 +5,17 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-// Check if a user with the given username already exists
-const doesExist = (username) => {
-    // Filter the users array for any user with the same username
-    let userswithsamename = users.filter((user) => {
-        return user.username === username;
-    });
-    // Return true if any user with the same username is found, otherwise false
-    if (userswithsamename.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-    // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-    // Check if both username and password are provided
     if (username && password) {
-        // Check if the user does not already exist
-        if (!doesExist(username)) {
-            // Add the new user to the users array
+        if (isValid(username)) {
             users.push({"username": username, "password": password});
             return res.status(200).json({message: "User successfully registered. Now you can login"});
         } else {
             return res.status(404).json({message: "User already exists!"});
         }
     }
-    // Return error if username or password is missing
     return res.status(404).json({message: "Unable to register user."});
 });
 
@@ -70,7 +38,6 @@ public_users.get('/author/:author',function (req, res) {
   const matchingBooks = bookArray.filter(book => 
     book.author.trim().toLowerCase() === author.trim().toLowerCase()
 );
-
 if (matchingBooks.length > 0) {
     return res.status(200).json({ 
         message: `Books found by author: ${author}`,
